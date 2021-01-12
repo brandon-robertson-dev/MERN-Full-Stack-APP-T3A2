a3817b36a5f749db0d8817b68a09fb3c6ce1eea2
Jese310S <jess.sole710@gmail.com>
2021-01-12 15:56:04 +1100

    fix alert bug with uuid

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 const Alert = ({ alerts }) =>  alerts !== null && alerts.lengh > 0 && alerts.map(alert => (
-    <div key={alert.id}className={`alert alert-${alert.alertType}`}>
+    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
         {alert.msg}
     </div>
 ))
