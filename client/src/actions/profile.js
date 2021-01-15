5cb6f6da14732f05383345f8aa285201a1156244
Jese310S <jess.sole710@gmail.com>
2021-01-15 19:50:13 +1100

    display profiles to developers tab

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -8,7 +8,8 @@ import {
     ACCOUNT_DELETED,
     CLEAR_PROFILE,
     GET_PROFILES,
-    GET_REPOS
+    GET_REPOS,
+    NO_REPOS
 } from './types';
 
 //Get current users profile
@@ -216,7 +217,7 @@ export const deleteEducation = (id) => async (dispatch) => {
 export const deleteAccount = () => async (dispatch) => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {
     try {
-      const res = await axios.delete('/api/profile');
+      await axios.delete('/api/profile');
 
       dispatch({ type: CLEAR_PROFILE });
       dispatch({ type: ACCOUNT_DELETED });
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -14,3 +14,4 @@ export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const GET_REPOS = 'GET_REPOS';
+export const NO_REPOS = 'NO_REPOS';
