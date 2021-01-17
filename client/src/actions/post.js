5f6b950f30d180a654f0794bf9b01df57c913bb4
Jese310S <jess.sole710@gmail.com>
2021-01-17 12:56:33 +1100

    add types to post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,6 +3,8 @@ import { setAlert } from './alert';
 import {
     GET_POSTS,
     POST_ERROR,
+    UPDATE_LIKES,
+    DELETE_POST
 } from './types';
 
 
@@ -23,3 +25,57 @@ export const getPosts = () => async dispatch => {
     });
     }
 }
+
+// Add Like
+export const addLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`api/posts/like/${id}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: {id, likes: res.data}
+          });
+
+    } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+    }
+}
+
+// Remove like
+export const removeLike = id => async dispatch => {
+    try {
+      const res = await axios.put(`/posts/unlike/${id}`);
+  
+      dispatch({
+        type: UPDATE_LIKES,
+        payload: { id, likes: res.data }
+      });
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+
+  // Delete post
+export const deletePost = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/${id}`);
+  
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      });
+  
+      dispatch(setAlert('Post Removed', 'success'));
+    } catch (err) {
+      dispatch({
+        type: POST_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
