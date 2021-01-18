10841b5e599b4ccabc0ebe44c790257e1753d9b8
Brandon Robertson <brandonrobertson23@gmail.com>
2021-01-18 13:00:34 +1100

    moved client folder inside server

--- server/server.js
+++ server/server.js
@@ -19,9 +19,9 @@ app.use('/api/profile', profileRoutes)
 app.use('/api/posts', postRoutes)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'))
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
